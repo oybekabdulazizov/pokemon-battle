bc0d111d3d1e9e29387ef17caa9e6666de3c51f3
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-12-23 21:59:53 +0100

    create Pokecard and style it up

---
+++ src/Pokecard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Pokecard.css';
+
+const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
+
+class Pokecard extends Component {
+    render() {
+        let imgSrc = `${POKE_API}/${this.props.id}.png`
+        return (
+            <div className="Pokecard">
+                <img src={imgSrc} alt={this.props.name}/>
+                <h1>{this.props.name}</h1>
+                <div>Type: {this.props.type}</div>
+                <div>Exp: {this.props.exp}</div>
+            </div>
+        )
+    }
+}
+
+export default Pokecard;
