578543a134c9230afe9cfea94344c2e6fcf25ea9
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-12-23 22:09:55 +0100

    create Pokedex and add 8 pokemons, wire it up with App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import Pokecard from './Pokecard';
+import Pokedex from './Pokedex';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Pokecard 
-        id={4}
-        name="Charmander"
-        type="fire"
-        exp={64}
-      />
+      <Pokedex />
     </div>
   );
 }
